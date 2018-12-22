c8d08a1adbd232a8ff0e5df2f31bb1db67741391
xlfe <felixb@gmail.com>
2018-12-23 09:51:32 +1100

    refactoring in progress

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject jonure "0.1.0-SNAPSHOT"
   :description "PJON in clojure"
-  :url "http://github.com/xlfe/jonure"
+  :url "http://github.com/xlfe/PJON-clojure"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
@@ -9,6 +9,6 @@
                  [clojure.java-time "0.3.2"]
                  [clj-serial "2.0.5"]
                  [org.clojure/core.async "0.4.474"]
-                 [org.clojure/clojure "1.9.0"]]
+                 [org.clojure/clojure "1.10.0"]]
   :main jonure.core)
 
--- src/jonure/core.clj
+++ src/jonure/core.clj
@@ -7,12 +7,19 @@
         [java-time]
         [serial.core]))
 
-(def BYTE_TIME_OUT (java-time/millis 50))
+(def TIME_OUT 50)
+(def BYTE_TIME_OUT (java-time/millis TIME_OUT))
 (def BUF_SIZE 512)
 
 (def serial-port "/dev/tty.wchusbserial1410")
 
-(defn make-handler [c] (fn [stream] (do (async/put! c [(java-time/instant) (.read stream)]))))
+(defn make-handler [c]
+  (fn [stream]
+    (do
+      (async/put! c
+                  (try
+                    [(java-time/instant) (.read stream)]
+                    (catch java.io.IOException e [e nil]))))))
 
 (defn open-port
  [serial-port c]
@@ -25,23 +32,73 @@
  (java-time/after? (java-time/instant) (java-time/plus inst timeout)))
 
 
-(defn -main
-  []
-  (async/go []
-    (let [c (async/chan)
-          buf-len BUF_SIZE
-          port (open-port serial-port c)
-          B (byte-array buf-len)]
-      (loop [i 0
-             prev-received (java-time/instant)
-             [received-ts received-byte] (async/<!! c)]
 
-        (if (= i 0)
-          (java.util.Arrays/fill B (byte 0)))
+(defn- triage
+ [c outgoing]
+ (alt!
+  (async/timeout TIME_OUT) :timeout
+  c ([received] {:byte received})))
 
-        (if
-          (has-timed-out prev-received BYTE_TIME_OUT)
-          (recur 0 (java-time/instant) [received-ts received-byte])
+(defn- send-packet
+ [port out-packet]
+ nil)
+
+(defn- wait-for-byte-or-outgoing
+ [c outgoing port]
+ (async/alt!
+  (async/timeout TIME_OUT) nil
+  c ([received] {:byte received})
+  outgoing ([out-packet]) (send-packet port out-packet)))
+
+(defn- wait-for-byte
+ [c]
+ (async/alt!
+  (async/timeout TIME_OUT) :timeout
+  c ([received] {:byte received})))
+
+(defn- transport-loop
+ [serial-port incoming outgoing]
+ (let [c (async/chan)
+       port (open-port serial-port c)
+       B (byte-array BUF_SIZE)]
+   (async/go-loop
+     [i 0]
+     (if-let [packet (packet/try-for-packet B i)]
+       (do
+         (serial.core/write port (byte 6))
+         (async/put! incoming {:packet packet})
+         (recur 0)))
+     (if-let [result (if (= i 0)
+                         (wait-for-byte-or-outgoing c outgoing port)
+                         (wait-for-byte c))]
+       (do
+         (aset-byte B i (unchecked-byte (:byte result)))
+         (recur (inc i)))
+       (recur 0)))))
+
+
+
+
+(defn create-channels
+  [serial-port]
+  (let [packets-received (async/chan)
+        packets-to-send (async/chan)
+        c (async/chan)
+        port (open-port serial-port c)
+        B (byte-array BUF_SIZE)]
+    (async/go-loop
+      [i 0
+       prev-received (java-time/instant)
+       [received-ts received-byte] (async/<!! c)]
+      (if (= i 0)
+        (java.util.Arrays/fill B (byte 0)))
+
+      (if (nil? received-byte)
+        (async/put! packets-received {:error received-ts} (fn [ret] (do (async/close! packets-received) (async/close! packets-to-send))))
+        (if (has-timed-out prev-received BYTE_TIME_OUT)
+          (do
+            ;timeout - check for outgoing packets
+            (recur 0 (java-time/instant) [received-ts received-byte]))
           (do
             (aset-byte B i (unchecked-byte received-byte))
             (let [packet (packet/try-for-packet B i)]
@@ -51,11 +108,21 @@
                    (not (nil? packet)))
                  (do
                   (serial.core/write port (byte 6))
-                  (byte-streams/print-bytes (:data packet))
-                  (println packet)
+                  (async/put! packets-received {:packet packet})
+                  ;packet received - check for outgoing
                   (recur 0 received-ts (async/<!! c)))
-                 (recur (inc i) received-ts (async/<!! c)))))))))
+                 ;packet reception in progress - don't check for outgoing
+                 (recur (inc i) received-ts (async/<!! c))))))))
+    [packets-received packets-to-send]))
 
-  (async/<!! (async/go-loop [n 0]
-                          (async/<! (async/timeout 10000))
-                          (recur (inc n)))))
+
+(defn -main
+  []
+  (let [[incoming outgoing] (create-channels serial-port)]
+    (async/<!! (async/go-loop
+                 [packet (async/<!! incoming)]
+                 (println packet)
+                 (if (contains? packet :packet)
+                   (do
+                     (byte-streams/print-bytes (:data (:packet packet)))
+                     (recur (async/<!! incoming))))))))
